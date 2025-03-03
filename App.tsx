dade83a9cb9dfeb1dd37a42fe32ed0c6a6ab92c0
antonio-jb <antonio_ajb@outlook.com>
2025-03-03 23:23:31 +0100

    Llamada a productos hecho

--- App.tsx
+++ App.tsx
@@ -3,9 +3,11 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import LoginScreen from "./app/presentation/views/auth/login";
 import RegistroScreen from "./app/presentation/views/auth/registro";
-import {ProfileInfoScreen} from "./app/presentation/views/profile/info/ProfileInfo";
 import {RolesScreen} from "./app/presentation/views/roles/RolesInfo";
 import {AdminTabNavigator} from "./app/presentation/navigation/AdminTabNavigator";
+import {useFonts} from "expo-font";
+import {ActivityIndicator} from "react-native";
+import {ClienteTabNavigator} from "./app/presentation/navigation/ClienteTabNavigator";
 
 export type RootStackParamList = {
     LoginScreen: undefined,
@@ -13,19 +15,20 @@ export type RootStackParamList = {
     ProfileInfoScreen: undefined,
     RolesScreen: undefined,
     AdminTabNavigator: undefined,
+    ClienteTabNavigator: undefined,
 }
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
     return (
         <NavigationContainer>
-
             <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name={"LoginScreen"} component={LoginScreen}></Stack.Screen>
                 <Stack.Screen name={"RegistroScreen"} component={RegistroScreen} options={{headerShown: true, title: "Registro"}}></Stack.Screen>
-                <Stack.Screen name={"AdminTabNavigator"} component={AdminTabNavigator} options={{title: "Navegación de administrador"}}></Stack.Screen>
+                <Stack.Screen name={"RolesScreen"} component={RolesScreen} options={{headerShown: true, title: "Roles"}}></Stack.Screen>
+                <Stack.Screen name={"AdminTabNavigator"} component={AdminTabNavigator} options={{title: "Navegacion de admin"}}></Stack.Screen>
+                <Stack.Screen name={"ClienteTabNavigator"} component={ClienteTabNavigator} options={{title: "Navegacion de cliente"}}></Stack.Screen>
             </Stack.Navigator>
-
         </NavigationContainer>
     );
 }
